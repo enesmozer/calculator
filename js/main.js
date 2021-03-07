110dd38056e5bc3b489240a1e97b29f19969b982
Enes Malik Özer <enesmozer@gmail.com>
2021-03-07 17:02:38 +0300

    refactor

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
+//#region  Variables
+let number = 0;
+const display = document.querySelector(".calculator-screen");
+const calculator = document.querySelector(".calculator");
+
+const keyTypes = Object.freeze( {
+  OPERATOR : 'operator',
+  EQUAL : 'equal',
+  NUMBER : 'number',
+  CLEAR : 'clear',
+  DECIMAL : 'decimal',
+  REVERSE : 'reverse',
+  PERCENT : 'percent'
+});
+
+const operators = Object.freeze( {
+  ADD : 'add',
+  SUBTRACT : 'subtract',
+  MULTIPLY : 'multiply',
+  DIVIDE : 'divide'
+});
+//#region Methods
 const calculate = (n1, operator, n2) => {
   let result = 0;
   n1 = parseFloat(n1);
@@ -18,108 +40,134 @@ const calculate = (n1, operator, n2) => {
     default:
       return result;
   }
-  isOperation = true;
   return result;
 };
+const createResultString = (pressedKey, displayedNum, state) => {
+  const keyType = getKeyType(pressedKey);
 
-let number = 0;
+  const { firstValue, operator, modValue, previousKeyType } = state;
 
-const display = document.querySelector(".calculator-screen");
-const calculator = document.querySelector(".calculator");
-const clear = document.getElementById("clear");
+  if (!isNaN(keyType)) {
+    return displayedNum === "0" ||
+      previousKeyType === keyTypes.OPERATOR ||
+      previousKeyType === keyTypes.EQUAL
+      ? pressedKey
+      : number + pressedKey;
+  }
 
-let isOperation = false;
-let isOperator = false;
-const updateResult = (value) => {
+  if (keyType === keyTypes.DECIMAL) {
+    debugger;
+    if (!displayedNum.includes(".")) return `${displayedNum}.`;
+    if (previousKeyType === keyTypes.OPERATOR || previousKeyType === keyTypes.EQUAL) {
+      return "0.";
+    }
+    return displayedNum;
+  }
+
+  if (pressedKey === keyTypes.CLEAR) {
+    return 0;
+  }
+  if (keyType === keyTypes.REVERSE) {
+    return calculate(number, "multiply", -1);
+  }
+
+  if (keyType === keyTypes.PERCENT) {
+    return calculate(number, "multiply", 0.01);
+  }
+
+  if (keyType === keyTypes.OPERATOR) {
+    return firstValue &&
+      operator &&
+      previousKeyType !== keyTypes.OPERATOR &&
+      previousKeyType !== keyTypes.EQUAL
+      ? calculate(firstValue, operator, displayedNum)
+      : displayedNum;
+  }
+
+  if (keyType === keyTypes.EQUAL) {
+    return firstValue
+      ? previousKeyType === keyTypes.EQUAL
+        ? calculate(displayedNum, operator, modValue)
+        : calculate(firstValue, operator, displayedNum)
+      : displayedNum;
+  }
+};
+
+const updateCalculatorState = (
+  pressedKey,
+  calculator,
+  calculatedValue,
+  displayedNum
+) => {
+  const keyType = getKeyType(pressedKey);
+  const {
+    firstValue,
+    operator,
+    modValue,
+    previousKeyType,
+  } = calculator.dataset;
+
+  calculator.dataset.previousKeyType = keyType;
+
+  if (keyType === keyTypes.OPERATOR) {
+    calculator.dataset.operator = pressedKey;
+    calculator.dataset.firstValue =
+      firstValue &&
+      operator &&
+      previousKeyType !== keyTypes.OPERATOR &&
+      previousKeyType !== keyTypes.EQUAL
+        ? calculatedValue
+        : displayedNum;
+  }
+
+  if (keyType === keyTypes.EQUAL) {
+    calculator.dataset.modValue =
+      firstValue && previousKeyType === keyTypes.EQUAL ? modValue : displayedNum;
+  }
+
+  if (keyType === keyTypes.CLEAR && clear.innerText === "AC") {
+    calculator.dataset.firstValue = "";
+    calculator.dataset.modValue = "";
+    calculator.dataset.operator = "";
+    calculator.dataset.previousKeyType = "";
+  }
+};
+
+const getKeyType = (key) => {
+  if (
+    key === operators.ADD ||
+    key === operators.SUBTRACT ||
+    key === operators.MULTIPLY ||
+    key === operators.DIVIDE
+  )
+    return keyTypes.OPERATOR;
+  return key;
+};
+
+const updateResult = (value, button = null) => {
+  if (button) {
+    const keyType = getKeyType(button.dataset.key);
+    Array.from(button.parentNode.children).forEach((k) =>
+      k.classList.remove("clicked")
+    );
+    if (keyType === keyTypes.OPERATOR) button.classList.add("clicked");
+    if (keyType === keyTypes.CLEAR && clear.innerText !== "AC") clear.innerText = "AC";
+    if (keyType !== keyTypes.CLEAR) {
+      clear.innerText = "CE";
+    }
+  }
   display.innerText = value;
 };
 
 updateResult(0);
 
+//#region Event
+
 document.querySelector(".calculator").addEventListener("click", (e) => {
-  const el = e.target;
+  const button = e.target;
   const displayedNum = display.textContent;
-
-  let pressedKey = el.dataset.key;
-
-  Array.from(el.parentNode.children).forEach((k) =>
-    k.classList.remove("clicked")
-  );
-
-  if (displayedNum === "0") {
-    clear.innerText = "C";
-  }
-  //debugger;
-  if (!isNaN(pressedKey)) {
-    if (displayedNum === "0" || isOperation) {
-      number = pressedKey;
-    } else {
-      number = number + pressedKey;
-    }
-    isOperation = false;
-    isOperator = true;
-  }
-  debugger;
-  if (
-    pressedKey === "add" ||
-    pressedKey === "subtract" ||
-    pressedKey === "multiply" ||
-    pressedKey === "divide"
-  ) {
-    const firstValue = calculator.dataset.firstValue;
-    const operator = calculator.dataset.operator;
-    const secondValue = displayedNum;
-    if (firstValue && operator && isOperator) {
-      const calcValue = calculate(firstValue, operator, secondValue);
-      number = calcValue;
-      calculator.dataset.firstValue = calcValue;
-    } else {
-      calculator.dataset.firstValue = displayedNum;
-    }
-    calculator.dataset.operator = pressedKey;
-    isOperation = true;
-    isOperator = false;
-  }
-  debugger;
-  switch (pressedKey) {
-    case "clear":
-      number = 0;
-      calculator.dataset.firstValue = "";
-      calculator.dataset.operator = "";
-      calculator.dataset.modValue ="";
-      clear.innerText = "AC";
-      break;
-    case "reverse":
-      number = calculate(number, "multiply", -1);
-      break;
-    case "percent":
-      number = calculate(number, "multiply", 0.01);
-      break;
-    case "decimal":
-      if (!displayedNum.includes(".")) {
-        number = number + ".";
-      } else if (isOperation) {
-        number = "0.";
-        isOperation = false;
-      }
-      break;
-    case "equal":
-      let firstValue = calculator.dataset.firstValue;
-      const operator = calculator.dataset.operator;
-      let secondValue = displayedNum;
-      if (firstValue) {
-        if (isOperator) {
-          firstValue = displayedNum;
-          secondValue = calculator.dataset.modValue;
-        }
-        number = calculate(firstValue, operator, secondValue);
-      }
-      calculator.dataset.modValue = secondValue;
-      isOperation = true;
-      isOperator = false;
-      break;
-  }
-
-  updateResult(number);
-  el.classList.add("clicked");
+  let pressedKey = button.dataset.key;
+  number = createResultString(pressedKey, displayedNum, calculator.dataset);
+  updateCalculatorState(pressedKey, calculator, number, displayedNum);
+  updateResult(number, button);
 });
